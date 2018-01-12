995212985af170df1acc3611bbb84a7c6d267cbf
wangqinglu-on-macos <18353184289@163.com>
2018-01-12 15:18:36 +0800

    1

--- test.c
+++ test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-//#include <wiringPi.h>
+#include <wiringPi.h>
 #define LED 0
 
 int main(){
-    //wiringPiSetup();
+    wiringPiSetup();
     printf("heheda\n");
+    wiringPiSetup();
     return 0;
 }
